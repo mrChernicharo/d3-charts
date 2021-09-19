72cb3ccd90eb0070d70f0f34358e522e5fd7eaf7
Felipe Chernicharo <string7dev@gmail.com>
2021-09-18 20:44:59 -0400

    grouped-bars stackedData

--- src/app/charts/grouped-bar-chart/grouped-bar-chart.component.ts
+++ src/app/charts/grouped-bar-chart/grouped-bar-chart.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 import * as d3 from 'd3';
 import { IGroupedBarItem } from 'src/app/utils/groupedBarsHelper';
 
@@ -6,12 +13,15 @@ import { IGroupedBarItem } from 'src/app/utils/groupedBarsHelper';
   selector: 'app-grouped-bar-chart',
   templateUrl: './grouped-bar-chart.component.html',
   styleUrls: ['./grouped-bar-chart.component.scss'],
+  // encapsulation: ViewEncapsulation.None,
 })
 export class GroupedBarChartComponent implements OnInit, OnChanges {
   @Input() dataSource: IGroupedBarItem[];
   @Input() availableWidth: number;
-  @Input() outterMargins: number;
+  @Input() outerMargins: number;
+  height = 400;
 
+  margins = { top: 20, bottom: 32, left: 60, right: 48 };
   constructor() {}
 
   ngOnInit(): void {
@@ -27,7 +37,27 @@ export class GroupedBarChartComponent implements OnInit, OnChanges {
   }
 
   drawChart() {
-    d3.select('#grouped-bar').append('svg').attr('class', 'svg').attr('width');
+    d3.select('#grouped-bar')
+      .append('svg')
+      .attr('class', 'svg')
+      .attr('height', this.height)
+      .attr('width', `${this.availableWidth - this.outerMargins}`)
+      .style('transform', `translateX(${this.outerMargins / 2}px)`)
+      .style('border', '1px solid forestgreen');
   }
-  updateChart() {}
+  updateChart() {
+    d3.select('.svg')
+      .attr('width', `${this.availableWidth - this.outerMargins}`)
+      .style('transform', `translateX(${this.outerMargins / 2}px)`);
+
+    const stackGen = d3
+      .stack()
+      .keys(['pizzas', 'shakes', 'burgers', 'salads'])
+      .order(d3.stackOrderNone)
+      .offset(d3.stackOffsetNone);
+
+    const series = stackGen([...this.dataSource] as any[]);
+
+    console.log(series);
+  }
 }
--- src/app/utils/groupedBarsHelper.ts
+++ src/app/utils/groupedBarsHelper.ts
@@ -1,172 +1,186 @@
 export interface IGroupedBarItem {
-  product: string;
-  price: number;
-  quantity: number;
+  pizzas: number;
+  shakes: number;
+  burgers: number;
+  salads: number;
   time: Date;
 }
 
-export const initialGroupedData = [
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 10,
-    time: new Date(2021, 7, 31),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 20,
-    time: new Date(2021, 7, 31),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 27,
-    time: new Date(2021, 7, 31),
-  },
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 9,
-    time: new Date(2021, 8, 1),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 24,
-    time: new Date(2021, 8, 1),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 32,
-    time: new Date(2021, 8, 1),
-  },
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 11,
-    time: new Date(2021, 8, 2),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 17,
-    time: new Date(2021, 8, 2),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 34,
-    time: new Date(2021, 8, 2),
-  },
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 14,
-    time: new Date(2021, 8, 3),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 20,
-    time: new Date(2021, 8, 3),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 39,
-    time: new Date(2021, 8, 3),
-  },
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 7,
-    time: new Date(2021, 8, 4),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 22,
-    time: new Date(2021, 8, 4),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 28,
-    time: new Date(2021, 8, 4),
-  },
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 14,
-    time: new Date(2021, 8, 5),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 19,
-    time: new Date(2021, 8, 5),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 30,
-    time: new Date(2021, 8, 5),
-  },
-
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 5,
-    time: new Date(2021, 8, 6),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 25,
-    time: new Date(2021, 8, 6),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 27,
-    time: new Date(2021, 8, 6),
-  },
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 6,
-    time: new Date(2021, 8, 7),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 21,
-    time: new Date(2021, 8, 7),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 22,
-    time: new Date(2021, 8, 7),
-  },
-  {
-    product: 'Pizza',
-    price: 50,
-    quantity: 10,
-    time: new Date(2021, 8, 8),
-  },
-  {
-    product: 'Shake',
-    price: 10,
-    quantity: 33,
-    time: new Date(2021, 8, 8),
-  },
-  {
-    product: 'Burger',
-    price: 20,
-    quantity: 28,
-    time: new Date(2021, 8, 8),
-  },
+export const initialGroupedData: IGroupedBarItem[] = [
+  { pizzas: 10, shakes: 20, burgers: 35, salads: 12, time: new Date(2021, 7, 29) },
+  { pizzas: 11, shakes: 24, burgers: 30, salads: 16, time: new Date(2021, 7, 30) },
+  { pizzas: 9, shakes: 22, burgers: 37, salads: 19, time: new Date(2021, 7, 31) },
+  { pizzas: 12, shakes: 18, burgers: 28, salads: 20, time: new Date(2021, 8, 1) },
+  { pizzas: 13, shakes: 20, burgers: 20, salads: 12, time: new Date(2021, 8, 2) },
+  { pizzas: 6, shakes: 20, burgers: 24, salads: 13, time: new Date(2021, 8, 3) },
+  { pizzas: 10, shakes: 21, burgers: 25, salads: 16, time: new Date(2021, 8, 4) },
+  { pizzas: 12, shakes: 19, burgers: 35, salads: 22, time: new Date(2021, 8, 5) },
+  { pizzas: 8, shakes: 16, burgers: 39, salads: 11, time: new Date(2021, 8, 6) },
+  { pizzas: 9, shakes: 24, burgers: 32, salads: 17, time: new Date(2021, 8, 7) },
 ];
+
+// export const initialGroupedData = [
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 10,
+//     time: new Date(2021, 7, 31),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 20,
+//     time: new Date(2021, 7, 31),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 27,
+//     time: new Date(2021, 7, 31),
+//   },
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 9,
+//     time: new Date(2021, 8, 1),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 24,
+//     time: new Date(2021, 8, 1),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 32,
+//     time: new Date(2021, 8, 1),
+//   },
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 11,
+//     time: new Date(2021, 8, 2),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 17,
+//     time: new Date(2021, 8, 2),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 34,
+//     time: new Date(2021, 8, 2),
+//   },
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 14,
+//     time: new Date(2021, 8, 3),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 20,
+//     time: new Date(2021, 8, 3),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 39,
+//     time: new Date(2021, 8, 3),
+//   },
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 7,
+//     time: new Date(2021, 8, 4),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 22,
+//     time: new Date(2021, 8, 4),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 28,
+//     time: new Date(2021, 8, 4),
+//   },
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 14,
+//     time: new Date(2021, 8, 5),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 19,
+//     time: new Date(2021, 8, 5),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 30,
+//     time: new Date(2021, 8, 5),
+//   },
+
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 5,
+//     time: new Date(2021, 8, 6),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 25,
+//     time: new Date(2021, 8, 6),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 27,
+//     time: new Date(2021, 8, 6),
+//   },
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 6,
+//     time: new Date(2021, 8, 7),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 21,
+//     time: new Date(2021, 8, 7),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 22,
+//     time: new Date(2021, 8, 7),
+//   },
+//   {
+//     product: 'Pizza',
+//     price: 50,
+//     quantity: 10,
+//     time: new Date(2021, 8, 8),
+//   },
+//   {
+//     product: 'Shake',
+//     price: 10,
+//     quantity: 33,
+//     time: new Date(2021, 8, 8),
+//   },
+//   {
+//     product: 'Burger',
+//     price: 20,
+//     quantity: 28,
+//     time: new Date(2021, 8, 8),
+//   },
+// ];
